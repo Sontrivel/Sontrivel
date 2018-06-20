e7448ee787d21d0549ba0c4ac2cc6ce1f048641e
Sontrivel <piligrin106@gmail.com>
2018-06-20 10:44:09 +0300

    release v1"

---
+++ app/Action.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Action extends Model
+{
+    public $table = 'action';
+    public function depends_on() {
+        return $this->hasMany('App\DependsOn');
+    }
+    public function depends_from() {
+        return $this->hasMany('App\DependsFrom');
+    }
+    public function description_and_actions() {
+        return $this->hasMany('App\DescriptionAndActions');
+    }
+    public function has_actions() {
+        return $this->hasMany('App\Action');
+    }
+    public function opposite_actions() {
+        return $this->belongsTo('App\Action');
+    }
+}
